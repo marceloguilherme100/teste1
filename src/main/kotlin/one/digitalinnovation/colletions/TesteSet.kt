69db5c7912090e84692914d6a57e18a9d7cb05f3
Perkles <marcelo.guilherme100@gmail.com>
2021-07-26 00:23:41 -0300

    created string array of test class

---
+++ src/main/kotlin/one/digitalinnovation/colletions/TesteSet.kt
@@ -0,0 +1,29 @@
+package one.digitalinnovation.colletions
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+
+    val funcionarios1 = setOf(joao, pedro)
+    val funcionarios2 = setOf(maria)
+
+    val resultUnion = funcionarios1.union(funcionarios2)
+    resultUnion.forEach{ println(it) }
+
+    println("-------------------------")
+    val funcionarios3 = setOf(joao, pedro, maria)
+    val resutIntersect = funcionarios3.subtract(funcionarios2)
+    resutIntersect.forEach { println(it) }
+
+    println("-------------------------")
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach { println(it) }
+
+
+}
+
+
+
+
